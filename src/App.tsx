87b1ba2913a9ab019de383dd3013118faaed8d87
Primo <zpdong93@gmail.com>
2019-04-04 15:30:24 +0800

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Learn More</h1>
+        </header>
+        <p className="App-intro">
+          <a href="https://primoziv.github.io/gofar">Gofar</a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
